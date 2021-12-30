abe0b8ca07940f4b43828e8fdf48671985c952da
Paul Bernier <bernier.pja@gmail.com>
2021-12-30 13:31:51 -0800

    refactor rpc-client

---
+++ src/rpc-client.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+export class RpcClient {
+  private _endpoint: string;
+
+  constructor(endpoint: string) {
+    this._endpoint = endpoint;
+  }
+
+  async call(method: string, params: any): Promise<void> {
+    const data = {
+      jsonrpc: "2.0",
+      id: 0,
+      method: method,
+      params: params,
+    };
+
+    return axios
+      .post(this._endpoint, data)
+      .then((r) => {
+        const { error, result } = r.data;
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(result);
+        }
+      })
+      .catch((error) => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+      });
+  }
+}
