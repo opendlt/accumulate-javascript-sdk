b6998c43a3074a8de5b50f19375e4d3a35795f02
Paul Bernier <bernier.pja@gmail.com>
2022-01-22 11:41:42 -0800

    add quick start guide in README

--- src/lite-account.ts
+++ src/lite-account.ts
@@ -3,6 +3,9 @@ import { AccURL, ACME_TOKEN_URL } from "./acc-url";
 import { Keypair } from "./keypair";
 import { KeypairSigner } from "./keypair-signer";
 
+/**
+ * A LiteAccount controlled by an in-memory keypair
+ */
 export class LiteAccount extends KeypairSigner {
   private readonly _tokenUrl: AccURL;
 
@@ -11,14 +14,23 @@ export class LiteAccount extends KeypairSigner {
     this._tokenUrl = AccURL.toAccURL(tokenUrl);
   }
 
+  /**
+   * Generate a new random LiteAccount for the ACME token
+   */
   static generate(): LiteAccount {
     return new LiteAccount(ACME_TOKEN_URL, new Keypair());
   }
 
+  /**
+   * Generate a new LiteAccount for the ACME token with the given keypair
+   */
   static generateWithKeypair(keypair: Keypair): LiteAccount {
     return new LiteAccount(ACME_TOKEN_URL, keypair);
   }
 
+  /**
+   * Generate a new random LiteAccount for the given token URL
+   */
   static generateWithTokenUrl(tokenUrl: AccURL): LiteAccount {
     return new LiteAccount(tokenUrl, new Keypair());
   }
@@ -31,6 +43,9 @@ export class LiteAccount extends KeypairSigner {
     return this._tokenUrl;
   }
 
+  /**
+   * Compute a LiteAccount URL based on publickey and token URL.
+   */
   static computeUrl(publicKey: Uint8Array, tokenUrl: AccURL): AccURL {
     const pkHash = sha256(publicKey).slice(0, 20);
     const checkSum = sha256(pkHash.toString("hex")).slice(28);
