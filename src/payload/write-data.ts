fed21e182a924ab9582bf985bb53ca918399d2bc
Paul Bernier <bernier.pja@gmail.com>
2022-04-30 19:54:36 -0700

    feat: add new 'scratch' field to WriteData

--- src/payload/write-data.ts
+++ src/payload/write-data.ts
@@ -1,17 +1,25 @@
-import { bytesMarshalBinary, fieldMarshalBinary, uvarintMarshalBinary } from "../encoding";
+import {
+  booleanMarshalBinary,
+  bytesMarshalBinary,
+  fieldMarshalBinary,
+  uvarintMarshalBinary,
+} from "../encoding";
 import { TransactionType } from "../tx-types";
 import { BasePayload } from "./base-payload";
 
 export type WriteDataArg = {
   data: Uint8Array[];
+  scratch?: boolean;
 };
 
 export class WriteData extends BasePayload {
   private readonly _data: Uint8Array[];
+  private readonly _scratch: boolean;
 
   constructor(arg: WriteDataArg) {
     super();
     this._data = arg.data;
+    this._scratch = arg.scratch || false;
   }
 
   protected _marshalBinary(): Buffer {
@@ -20,6 +28,9 @@ export class WriteData extends BasePayload {
     forConcat.push(uvarintMarshalBinary(TransactionType.WriteData, 1));
 
     forConcat.push(fieldMarshalBinary(2, marshalDataEntry(this._data)));
+    if (this._scratch) {
+      forConcat.push(booleanMarshalBinary(this._scratch, 3));
+    }
 
     return Buffer.concat(forConcat);
   }
