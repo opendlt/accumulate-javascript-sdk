2c897e75e758d25b733a308f709b89b68ded73d2
Paul Bernier <bernier.pja@gmail.com>
2022-08-16 02:51:09 +0000

    feat: add 'proof' parameter to create token account

--- src/acc-url.ts
+++ src/acc-url.ts
@@ -72,3 +72,18 @@ export class AccURL {
     return this._url.toString();
   }
 }
+
+/**
+ * The URL of the ACME token
+ */
+export const ACME_TOKEN_URL = AccURL.parse("acc://ACME");
+
+/**
+ * The URL of the DN
+ */
+export const DN_URL = AccURL.parse("acc://dn.acme");
+
+/**
+ * The URL of the anchors
+ */
+export const ANCHORS_URL = DN_URL.append("anchors");
