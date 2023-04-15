bcf78e3bb3efe39f228c7d1cccf90606cfdbfa5e
Ethan Reesor <ethan.reesor@gmail.com>
2023-04-14 23:13:25 -0500

    Make typedoc shut up

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import BN from "bn.js";
+/* eslint-disable @typescript-eslint/no-namespace */
 export * from "./acc-url";
+export * from "./url";
 export * from "./client";
-export { Transaction, TransactionHeader as Header, TransactionType } from "./core";
 export * from "./encoding";
+export * from "./signing";
+export * as api_v2 from "./api_v2";
+export * as core from "./core";
+export * as errors from "./errors";
+export * as merkle from "./merkle";
+export * as messaging from "./messaging";
+export * as network from "./network";
+
 export { RpcError } from "./rpc-client";
 export { constructIssuerProof } from "./util";
+
+import BN from "bn.js";
 export { BN };
