25eb9b72187001668d317e0abfaf02ca79990a24
Ethan Reesor <ethan.reesor@gmail.com>
2023-05-02 15:19:11 -0500

    External signer example

--- src/signing/ed25519.ts
+++ src/signing/ed25519.ts
@@ -2,9 +2,13 @@ import nacl from "tweetnacl";
 import { PrivateKeyAddress } from "../address";
 import { sha256 } from "../common/crypto";
 import { SignatureType } from "../core";
-import { BaseKey } from "./key";
+import { BaseKey, PrivateKey, PublicKey } from "./key";
 
 abstract class BaseED25519Key extends BaseKey {
+  protected constructor(public readonly address: PrivateKey) {
+    super(address);
+  }
+
   protected static async make(type: SignatureType, seedOrKey?: Uint8Array) {
     let kp: nacl.SignKeyPair;
     if (!seedOrKey) {
@@ -45,5 +49,39 @@ export class RCD1Key extends BaseED25519Key {
 
   static async from(seedOrKey: Uint8Array) {
     return new this(await this.make(SignatureType.RCD1, seedOrKey));
+  }
+}
+
+export class ExternalED22519Key extends BaseKey {
+  constructor(
+    address: PublicKey,
+    private readonly _sign: (hash: Uint8Array) => Promise<Uint8Array>
+  ) {
+    super(address);
+    if (address.type != SignatureType.ED25519) {
+      throw new Error(`address is ${address.type}, not ED25519`);
+    }
+  }
+
+  async signRaw(args: { message: Uint8Array; sigMdHash: Uint8Array }): Promise<Uint8Array> {
+    const hash = await sha256(Buffer.concat([args.sigMdHash, args.message]));
+    return this._sign(hash);
+  }
+}
+
+export class ExternalRCD1Key extends BaseKey {
+  constructor(
+    address: PublicKey,
+    private readonly _sign: (hash: Uint8Array) => Promise<Uint8Array>
+  ) {
+    super(address);
+    if (address.type != SignatureType.RCD1) {
+      throw new Error(`address is ${address.type}, not RCD1`);
+    }
+  }
+
+  async signRaw(args: { message: Uint8Array; sigMdHash: Uint8Array }): Promise<Uint8Array> {
+    const hash = await sha256(Buffer.concat([args.sigMdHash, args.message]));
+    return this._sign(hash);
   }
 }
