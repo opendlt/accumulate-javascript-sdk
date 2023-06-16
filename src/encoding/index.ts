ab18b6338b69b6c8c239f464ffa23a075b5d9e94
Ethan Reesor <ethan.reesor@gmail.com>
2023-06-16 18:31:08 -0500

    Provide hooks into the encoder

--- src/encoding/index.ts
+++ src/encoding/index.ts
@@ -3,44 +3,65 @@
 
 import "reflect-metadata";
 import {
-  bigNumberMarshalBinary as bigIntMarshalBinary,
-  booleanMarshalBinary,
-  bytesMarshalBinary,
-  hashMarshalBinary,
-  stringMarshalBinary,
-  uvarintMarshalBinary as uintMarshalBinary,
-  varintMarshalBinary as intMarshalBinary,
-} from "./encoding";
+  BigInt,
+  Bool,
+  Bytes,
+  Encodable,
+  Enum,
+  Hash,
+  Int,
+  RawJson,
+  Reference,
+  String,
+  Time,
+  TxID,
+  Uint,
+  Union,
+  Url,
+} from "./encodable";
+import { bytesMarshalBinary, uvarintMarshalBinary as uintMarshalBinary } from "./encoding";
+
+export const FieldNumber = new Uint();
+export const Length = new Uint();
 
 export function encode(target: any) {
-  const enc = Encoding.get(target);
-  if (!enc) throw new Error("cannot encode object: no metadata");
-
   const parts: Buffer[] = [];
-  for (const field of enc.fields) {
-    encodeField(target, field, parts);
-  }
+  consume(target, (field, value) => {
+    parts.push(uintMarshalBinary(field.number));
+    parts.push(field.type.encode(value));
+  });
 
   return Buffer.concat(parts);
 }
 
-function encodeField(target: any, field: Field, parts: Buffer[]) {
-  const value = field.type == Embedded ? target : target[field.name];
+export type Consumer = (field: Field, value: any) => void;
+
+export function consume(target: any, consume: Consumer) {
+  const enc = Encoding.get(target);
+  if (!enc) throw new Error("cannot encode object: no metadata");
+
+  for (const field of enc.fields) {
+    encodeField(target, field, consume);
+  }
+}
+
+function encodeField(target: any, field: Field, consume: Consumer) {
+  const value = field.type instanceof Embedded ? target : target[field.name];
   if (!field.repeatable) {
-    encodeValue(value, field, parts);
+    encodeValue(value, field, consume);
     return;
   }
 
-  if (value) for (const item of value) encodeValue(item, field, parts);
+  if (value) for (const item of value) encodeValue(item, field, consume);
 }
 
-function encodeValue(value: any, field: Field, parts: Buffer[]) {
-  switch (field.type) {
-    case Hash:
+function encodeValue(value: any, field: Field, consume: Consumer) {
+  switch (true) {
+    case field.type instanceof Hash:
       if (!field.keepEmpty && isZeroHash(value)) return;
       break;
 
-    case Embedded:
+    case field.type instanceof Embedded:
       break;
 
     default:
@@ -48,87 +69,7 @@ function encodeValue(value: any, field: Field, parts: Buffer[]) {
       break;
   }
 
-  parts.push(uintMarshalBinary(field.number));
-
-  switch (field.type) {
-    case Int:
-      parts.push(intMarshalBinary(value));
-      break;
-
-    case Uint:
-      parts.push(uintMarshalBinary(value));
-      break;
-
-    case Bool:
-      parts.push(booleanMarshalBinary(value));
-      break;
-
-    case String:
-      parts.push(stringMarshalBinary(value));
-      break;
-
-    case Hash:
-      parts.push(hashMarshalBinary(value));
-      break;
-
-    case Bytes:
-      parts.push(bytesMarshalBinary(value));
-      break;
-
-    case Url:
-      parts.push(stringMarshalBinary(value.toString()));
-      break;
-
-    case Time:
-      parts.push(uintMarshalBinary((<Date>value).getTime() / 1000));
-      break;
-
-    case Duration:
-      throw new Error("cannot marshal duration to binary");
-
-    case BigInt:
-      parts.push(bigIntMarshalBinary(value));
-      break;
-
-    case Any:
-      throw new Error("cannot marshal type any to binary");
-
-    case RawJson:
-      {
-        const bytes = Buffer.from(JSON.stringify(value), "utf-8");
-        parts.push(bytesMarshalBinary(bytes));
-      }
-      break;
-
-    case Float:
-      throw new Error("cannot marshal float to binary");
-
-    case Txid:
-      parts.push(stringMarshalBinary(value.toString()));
-      break;
-
-    case Enum:
-      parts.push(uintMarshalBinary(value));
-      break;
-
-    case Union:
-    case Reference:
-      parts.push(bytesMarshalBinary(encode(value)));
-      break;
-
-    case Embedded:
-      {
-        const sub: Buffer[] = [];
-        for (const item of field.embedded!) {
-          encodeField(value, item, sub);
-        }
-        parts.push(bytesMarshalBinary(Buffer.concat(sub)));
-      }
-      break;
-
-    default:
-      throw new Error("internal error - invalid type code");
-  }
+  consume(field, value);
 }
 
 function isZeroHash(value: any) {
@@ -139,33 +80,29 @@ function isZeroHash(value: any) {
   return true;
 }
 
-const Int = 1;
-const Uint = 2;
-const Bool = 3;
-const String = 4;
-const Hash = 5;
-const Bytes = 6;
-const Url = 7;
-const Time = 8;
-const Duration = 9;
-const BigInt = 10;
-const Any = 11;
-const RawJson = 12;
-const Float = 13;
-const Txid = 14;
-
-const Reference = 1001;
-const Enum = 1002;
-const Union = 1003;
-const Embedded = 1004;
-
-interface Field {
+export interface Field {
   name: PropertyKey;
   number: number;
-  type: number;
+  type: Encodable;
   repeatable: boolean;
   embedded?: Field[];
   keepEmpty: boolean;
+}
+
+class Embedded {
+  constructor(private readonly field: Field) {}
+
+  encode(value: any) {
+    const parts: Buffer[] = [];
+    const consumer: Consumer = (field, value) => {
+      parts.push(uintMarshalBinary(field.number));
+      parts.push(field.type.encode(value));
+    };
+    for (const item of this.field.embedded!) {
+      encodeField(value, item, consumer);
+    }
+    return bytesMarshalBinary(Buffer.concat(parts));
+  }
 }
 
 class Encoding {
@@ -199,13 +136,17 @@ class Encoding {
     if (!field) {
       field = {
         number: number[0],
-        type: Embedded,
+        type: {
+          encode() {
+            throw new Error("placeholder");
+          },
+        },
         name: "",
         repeatable: false,
         keepEmpty: false,
         embedded: [],
       };
-      this.fields.push(field);
+      (field.type = new Embedded(field)), this.fields.push(field);
     }
 
     field.embedded!.push({ number: number[1], ...props });
@@ -240,82 +181,86 @@ export class Annotator {
 
   get enum() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: Enum, ...this.field });
+      Encoding.set(target).addField(this.number, { name: key, type: new Enum(), ...this.field });
     };
   }
   get reference() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: Reference, ...this.field });
+      Encoding.set(target).addField(this.number, {
+        name: key,
+        type: new Reference(),
+        ...this.field,
+      });
     };
   }
   get union() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: Union, ...this.field });
+      Encoding.set(target).addField(this.number, { name: key, type: new Union(), ...this.field });
     };
   }
 
   get int() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: Int, ...this.field });
+      Encoding.set(target).addField(this.number, { name: key, type: new Int(), ...this.field });
     };
   }
   get uint() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: Uint, ...this.field });
+      Encoding.set(target).addField(this.number, { name: key, type: new Uint(), ...this.field });
     };
   }
   get bool() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: Bool, ...this.field });
+      Encoding.set(target).addField(this.number, { name: key, type: new Bool(), ...this.field });
     };
   }
   get string() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: String, ...this.field });
+      Encoding.set(target).addField(this.number, { name: key, type: new String(), ...this.field });
     };
   }
   get hash() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: Hash, ...this.field });
+      Encoding.set(target).addField(this.number, { name: key, type: new Hash(), ...this.field });
     };
   }
   get bytes() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: Bytes, ...this.field });
+      Encoding.set(target).addField(this.number, { name: key, type: new Bytes(), ...this.field });
     };
   }
   get url() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: Url, ...this.field });
+      Encoding.set(target).addField(this.number, { name: key, type: new Url(), ...this.field });
     };
   }
   get time() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: Time, ...this.field });
+      Encoding.set(target).addField(this.number, { name: key, type: new Time(), ...this.field });
     };
   }
   // asDuration(target: any, key: PropertyKey) {
-  //   Encoding.set(target).addField(this.number, { name: key, type: Duration, ...this.field });
+  //   Encoding.set(target).addField(this.number, { name: key, type: new Duration, ...this.field });
   // }
   get bigInt() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: BigInt, ...this.field });
+      Encoding.set(target).addField(this.number, { name: key, type: new BigInt(), ...this.field });
     };
   }
   // asAny(target: any, key: PropertyKey) {
-  //   Encoding.set(target).addField(this.number, { name: key, type: Any, ...this.field });
+  //   Encoding.set(target).addField(this.number, { name: key, type: new Any, ...this.field });
   // }
   get rawJson() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: RawJson, ...this.field });
+      Encoding.set(target).addField(this.number, { name: key, type: new RawJson(), ...this.field });
     };
   }
   // asFloat(target: any, key: PropertyKey) {
-  //   Encoding.set(target).addField(this.number, { name: key, type: Float, ...this.field });
+  //   Encoding.set(target).addField(this.number, { name: key, type: new Float, ...this.field });
   // }
   get txid() {
     return (target: any, key: PropertyKey) => {
-      Encoding.set(target).addField(this.number, { name: key, type: Txid, ...this.field });
+      Encoding.set(target).addField(this.number, { name: key, type: new TxID(), ...this.field });
     };
   }
 }
