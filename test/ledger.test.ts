4d1fb34492c8a05fc83bff02cf90c0ce37632e1f
Ethan Reesor <ethan.reesor@gmail.com>
2023-07-28 16:13:29 -0500

    Add ledger implementation of Key interface

--- test/ledger.test.ts
+++ test/ledger.test.ts
@@ -5,6 +5,7 @@ import {
   LedgerSignature,
   LedgerVersion,
 } from "../lib/ledger/model/results";
+import { AddCredits, Transaction } from "../src/core";
 import Accumulate from "../src/ledger";
 
 test("getAppName", async () => {
@@ -99,4 +100,38 @@ test("signTransaction for ACME type", async () => {
     false
   );
   expect(result.signature).toEqual(s.signature);
+});
+
+test.skip("sign transaction with builder", async () => {
+  const txn = new Transaction({
+    header: {
+      principal: "acc://lite-token-account.acme",
+      initiator: "b141868fcfc222e6d6b37a2aa8fe621e26f1a83d816b9d3cfc08a31898bdf5cb",
+    },
+    body: new AddCredits({
+      recipient: "acc://adi.acme/page",
+      amount: "200000",
+      oracle: 50000000,
+    }),
+  });
+
+  // TODO I assume this needs to be changed to accommodate the getPublicKey call?
+  const Ledger = new Accumulate(
+    await openTransportReplayer(
+      RecordStore.fromString(`
+    => e006008015058000002c80000119800000008000000080000000
+    <= 9000
+    => e0060100d5016c01020220e55d973bf691381c94602354d1e1f655f7b1c4bd56760dffeffa2bef4541ec11041d6163633a2f2f6c6974652d746f6b656e2d6163636f756e742e61636d65050106d285d8cc0408039ce385b1c487a77139e19c296fdd63ae7e149260bc3853b245f8eec562c62603650140011d6163633a2f2f6c6974652d746f6b656e2d6163636f756e742e61636d6502b141868fcfc222e6d6b37a2aa8fe621e26f1a83d816b9d3cfc08a31898bdf5cb0221010e02136163633a2f2f6164692e61636d652f706167650303030d400480e1eb17
+    <= 4050edaa8520374aa560845d797a35309caadd823427f4218c1ede364445b884d19671ab5546ab026c388887ec2986ef95e7eef71239bf1c5fc7e2eb040390dc0f009000
+    `)
+    )
+  );
+
+  const signer = await Ledger.signerForPage("acc://lite-token-account.acme", "44'/281'/0'/0'/0'");
+  const sig = await signer.withVersion(1).sign(txn, { timestamp: 1234567890 });
+  const s = new LedgerSignature(
+    "50edaa8520374aa560845d797a35309caadd823427f4218c1ede364445b884d19671ab5546ab026c388887ec2986ef95e7eef71239bf1c5fc7e2eb040390dc0f",
+    false
+  );
+  expect(Buffer.from(<ArrayBuffer>sig.signature).toString("hex")).toEqual(s.signature);
 });
